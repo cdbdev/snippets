034df49756ee90b11c8192a8504c6304d4fce178
cdbdev <39485622+cdbdev@users.noreply.github.com>
2020-01-30 14:10:55 +0100

    Create routes.ts

---
+++ html/jwt_client/src/routes.ts
@@ -0,0 +1,7 @@
+import LoginView from './comp/LoginView.vue';
+import MainView from './comp/MainView.vue';
+
+export default [ 
+	{ path: '/' , component: LoginView },
+	{ path: '/main', component: MainView }
+]
